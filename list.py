41d78f5283fa6fdec4a8a55baaf5e621bf76c7c8
LifeDream123 <63576649+LifeDream123@users.noreply.github.com>
2025-09-01 12:00:04 +0530

    Update list.py

--- list.py
+++ list.py
@@ -11,7 +11,7 @@ else:
     print("student not found in data")
 
 
-Task 2: Demonstrate List Slicing 
+#Task 2: Demonstrate List Slicing 
 
 mylist=[1,2,3,4,5,6,7,8,9,10]
 print("Origina list :",mylist)
@@ -19,6 +19,7 @@ extrat_list=mylist[:5]
 reverse_list=extrat_list[::-1]
 print("Exytracted First Five Elements:",extrat_list)
 print("Reversed of Extracted First Five Element:",reverse_list)
+
 
 
 
